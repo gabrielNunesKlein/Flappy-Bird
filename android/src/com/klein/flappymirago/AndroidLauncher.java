07d9dc25a948a04e0917a62ee224d97259a02dfb
gabrielNunesKlein <gabrielgnk@outlook.com>
2021-07-01 16:08:42 -0300

    Projeto criado

---
+++ android/src/com/klein/flappymirago/AndroidLauncher.java
@@ -0,0 +1,14 @@
+package com.klein.flappymirago;
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.klein.flappymirago.FlappyBird;
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FlappyBird(), config);
+	}
+}
